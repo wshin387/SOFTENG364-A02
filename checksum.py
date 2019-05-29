9f8dbc72543e8b89e0048f5990054c6b7be566d9
william-shin-387 <38583073+william-shin-387@users.noreply.github.com>
2019-05-29 20:51:54 +1200

    initial commit

---
+++ checksum.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+def internet_checksum(data, total=0x0):
+    '''
+    Internet Checksum of a bytes array.
+    Further reading:
+    1. https://tools.ietf.org/html/rfc1071
+    2. http://www.netfor2.com/checksum.html
+    '''
+	pass # TODO: Implement this function and return the checksum
---
+++ routing.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def forwarding(predecessor, source):
+    """ 
+    Compute a forwarding table from a predecessor list. 
+    """
+	pass  # TODO
+	
+	
+def dijkstra_generalized(graph, source, weight='weight', 
+                         infinity=None, 
+                         plus=None,
+                         less=None,
+                         min=None):
+    """
+    Least-cost or widest paths via Dijkstra's algorithm.
+    """
+	pass  # TODO: Please work from Lab 1 code
