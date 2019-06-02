ad8f154e7b2444ef23f80497bca2a78ea527e874
william-shin-387 <38583073+william-shin-387@users.noreply.github.com>
2019-06-02 15:11:26 +1200

    q1 + running file

--- routing.py
+++ routing.py
@@ -4,15 +4,54 @@ def forwarding(predecessor, source):
     """ 
     Compute a forwarding table from a predecessor list. 
     """
-	pass  # TODO
-	
-	
-def dijkstra_generalized(graph, source, weight='weight', 
-                         infinity=None, 
+    table = {}
+    for node in predecessor:
+        current = node
+        if current != source:
+            while predecessor[current][0] != source:
+                node = predecessor[current][0]
+            table[node] = (source, current)
+    return table
+
+
+def dijkstra_generalized(graph, source, weight='weight',
+                         infinity=None,
                          plus=None,
                          less=None,
                          min=None):
     """
     Least-cost or widest paths via Dijkstra's algorithm.
     """
-	pass  # TODO: Please work from Lab 1 code
+    import math
+    # Definitions consistent with Kurose & Ross
+    u = source
+
+    def c(x, y):
+        return graph[x][y][weight]
+
+    N = frozenset(graph.nodes())
+    NPrime = {u}  # i.e. "set([u])"
+    D = dict.fromkeys(N, math.inf)
+    pre = dict.fromkeys(N, [u])
+
+    # Initialization
+    for v in N:
+        if graph.has_edge(u, v):
+            D[v] = c(u, v)
+    D[u] = 0  # over-write inf entry for source
+    pre[u] = [] #over-write predecessor u
+
+    # Loop
+    while NPrime != N:
+        candidates = {w: D[w] for w in N if w not in NPrime}
+    w, Dw = min(candidates.items(), key=lambda item: item[1])
+    NPrime.add(w)
+    for v in graph[w]:
+        if v not in NPrime:
+            DvNew = D[w] + c(w, v)
+            if DvNew < D[v]:
+                D[v] = DvNew
+                pre[v] = [w]
+
+
+    return D, pre
